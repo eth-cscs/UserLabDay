83738d28790658998c694a56ce5a74d130fc2f70
rafael <rafael.sarmiento@cscs.ch>
2022-09-01 10:21:39 +0200

    update scripts

--- 2022/containers/ngc-bert-squad/run.sh
+++ 2022/containers/ngc-bert-squad/run.sh
@@ -1,24 +1,23 @@
 #!/bin/bash -l
 
 #SBATCH --job-name=bert-squad-finetune
-#SBATCH --time=00:50:00
+#SBATCH --time=00:30:00
 #SBATCH --nodes=8
 #SBATCH --ntasks-per-core=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=12
-##SBATCH --partition=debug
 #SBATCH --constraint=gpu
-#SBATCH --account=usup
+#SBATCH --account=<account>
 
 module load daint-gpu
 module load sarus
-module load OpenMPI/4.1.2
+module load OpenMPI/4.1.2   # installed locally by the user
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
-mpirun sarus run --tty --mount=type=bind,source=$SCRATCH,destination=$SCRATCH \
+mpirun sarus run --mount=type=bind,source=$SCRATCH,destination=$SCRATCH \
              nvcr.io/nvidia/pytorch:22.08-py3 \
              bash -c '
-             cd $SCRATCH/bert-demo;
+             cd $SCRATCH/UserLabDay/2022/containers/ngc-bert-squad
              . deepspeed-env/bin/activate;
-             python 3_squad_bert_deepspeed.py --deepspeed_config ds_config.json'
+             python bert_squad_deepspeed_train.py --deepspeed_config ds_config.json'
